9c3fc29872a9881d6165e3cda62d6dd06fb84541
jingjun <254032134@qq.com>
2021-07-03 00:14:26 +0800

    首页图片显示

--- miniproduct-1/pages/homepage/homepage.js
+++ miniproduct-1/pages/homepage/homepage.js
@@ -1,28 +1,34 @@
 // pages/homepage/homepage.js
-let page = 1;
-let size = 10
+
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    page: 1,
+    size: 10,
+    items: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.listNetworking(page)
+    this.listNetworking(this.data.page);
   },
 
-  listNetworking: function(page) {
+  listNetworking: function(e) {
+    this.data.page = e;
     var that = this;
+    if (this.data.page == 1) {
+      //在当前页面显示导航条加载动画
+      wx.showNavigationBarLoading(); 
+    }
     wx.request({
       url: 'https://test.rxswift.cn/api/v1/topiclist/',
       data:{
-        'page':page,
-        'size': size,
+        'page':that.data.page,
+        'size': that.data.size,
       },
       method: "POST",
       header: {
@@ -30,10 +36,30 @@ Page({
       },
       success(res) {
           console.log(res.data)
-          if (res.data['code'] == 200) {
-            that.setData({
-              items: res.data.data,
-            })
+          if (res.data['code'] == 200 && res.data['data'].length > 0) {
+            
+            if (that.data.page == 1) {
+              wx.hideNavigationBarLoading({
+                success: (res) => {},
+              })
+              wx.stopPullDownRefresh({
+                success: (res) => {},
+              })
+
+              that.setData({
+                items: res.data.data,
+              })
+              
+            }else{
+              var orginData = that.data.items;
+              that.setData({
+                items: orginData.concat(res.data.data)
+              })
+              // that.setData({
+              //   items: res.data.data,
+              // })
+            }
+            that.data.page += 1
           }
       }
     })
@@ -71,7 +97,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.listNetworking(1);
   },
 
   /**
@@ -87,4 +113,17 @@ Page({
   onShareAppMessage: function () {
 
   },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    this.onBottom();
+  },
+
+  //触底响应函数
+  onBottom(){
+    this.listNetworking(this.data.page);
+  },
+
 })
