18d0d29505c66c4588ed7a2fad124fb43dcd6fff
jingjun <254032134@qq.com>
2021-07-06 00:43:17 +0800

    创建帖子UI

--- miniproduct-1/pages/homeinfo/createtopic/createtopic.js
+++ miniproduct-1/pages/homeinfo/createtopic/createtopic.js
@@ -1,18 +1,35 @@
 // pages/HomeInfo/createtopic/createtopic.js
+/*
+  struct ReleasePhotoModel: HandyJSON, Equatable {
+    var image: UIImage?
+    var isAdd: Bool = false // 是不是添加的图片
+    var progress: Float = 0
+    var complete: Bool = false
+    var photoKey: String = "\(Tool.shared.getTime())/\(String.et.random(ofLength: 8)).jpeg"
+    var photoUrl: String = ""
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.photoKey == rhs.photoKey
+    }
+}
+*/
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    tags: null
+    tags: null,
+    photoModels:[{isAdd: true,progress: 0,complete: 0,photoKey: "",photoUrl: ""}]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      photoModels:[{isAdd: true,progress: 0,complete: 0,photoKey: "",photoUrl: ""}]
+    })
   },
 
   /**
@@ -68,6 +85,20 @@ Page({
   addTagsClick: function() {
     wx.navigateTo({
       url: '../selectedtags/selectedtags',
+    })
+  },
+
+  //用户名和密码输入框事件
+  contactInput:function(e){
+    console.log(e.detail.value);
+    this.setData({
+      contact:e.detail.value
+    })
+  },
+  bindTextAreaBlur: function(e) {
+    console.log(e.detail.value)
+    this.setData({
+      content: e.detail.value
     })
   }
 })
