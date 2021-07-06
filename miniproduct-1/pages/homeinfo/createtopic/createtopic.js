1a5b605686e25c0aa550552d36cbcdf419dd0274
jingjun <254032134@qq.com>
2021-07-07 00:25:04 +0800

    发布选择图片

--- miniproduct-1/pages/homeinfo/createtopic/createtopic.js
+++ miniproduct-1/pages/homeinfo/createtopic/createtopic.js
@@ -13,14 +13,15 @@
     }
 }
 */
+var emptyData = {isAdd: true,progress: 0,complete: 0,photoKey: "",photoUrl: ""};
 Page({
-
+  
   /**
    * 页面的初始数据
    */
   data: {
     tags: null,
-    photoModels:[{isAdd: true,progress: 0,complete: 0,photoKey: "",photoUrl: ""}]
+    photoModels:[emptyData]
   },
 
   /**
@@ -100,5 +101,64 @@ Page({
     this.setData({
       content: e.detail.value
     })
+  },
+  /**添加照片 */
+  addPhotoClick: function(e) {
+    console.log(e.currentTarget.dataset.id)
+    var item = e.currentTarget.dataset.id
+    var that = this;
+    var hadCount = this.data.photoModels.filter((num) => {
+      return num.isAdd != true;
+    });
+    console.log("hadCount: ",hadCount)
+    var count = 6 - hadCount.length;
+    console.log("count: ",count)
+    
+    wx.chooseImage({
+      count: count, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], // ['album', 'camera'] 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths);
+        // _this.setData({
+        //   imglist: _this.data.imglist.concat(tempFilePaths)
+        // })
+        var temp = tempFilePaths.map((num) => {
+          return {isAdd: false,progress: 0,complete: 0,photoKey: "",photoUrl: num}
+        })
+        console.log(temp)
+        var totalDatas = temp.concat(that.data.photoModels)
+        var photoDatas = totalDatas.filter((num) => {
+          return num.isAdd != true
+        })
+        if (photoDatas.length == 6) {
+          totalDatas = photoDatas 
+        }
+        that.setData({
+          photoModels: totalDatas
+        })
+      }
+    })
+  },
+  clearPhotoClick: function(e) {
+    var item = e.currentTarget.dataset.id
+    var totalPhotos = this.data.photoModels.filter((num) => {
+      return num.photoUrl != item.photoUrl
+    })
+
+    var emptyPhoto = totalPhotos.filter((num) => {
+      return num.isAdd == true
+    })
+
+    if (emptyPhoto.length == 0) {
+      totalPhotos.push({isAdd: true,progress: 0,complete: 0,photoKey: "",photoUrl: ""})
+    }
+    
+    this.setData({
+      photoModels: totalPhotos
+    })
+
   }
 })
