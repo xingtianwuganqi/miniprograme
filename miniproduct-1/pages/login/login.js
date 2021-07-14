f89ab3c4d25ecbee6d92bab52fa31146514fd194
jingjun <254032134@qq.com>
2021-07-15 00:32:21 +0800

    微信登录，保存用户信息

--- miniproduct-1/pages/login/login.js
+++ miniproduct-1/pages/login/login.js
@@ -1,4 +1,5 @@
 // pages/login/login.js
+const app = getApp().globalData;
 Page({
 
   /**
@@ -84,6 +85,69 @@ Page({
             console.log(userinfo);
           }
         })
+      }
+    })
+  },
+  /** 登录按钮点击 */
+  getUserInfo:function(){
+    if (this.data.isAgreen==false) {
+      console.log('=========')
+      this.openTextToast('请阅读并勾选用户协议与隐私协议');
+      return 
+    }
+    let that = this
+    //首先查看是否得到用户 的授权
+    wx.getSetting({
+      success:function(settingRes){
+        //console.log(res)
+        //res.authSetting['scope.userInfo']   代表用户授予权限的状态
+        console.log(settingRes.authSetting['scope.userInfo'])
+        if(settingRes.authSetting['scope.userInfo']){
+          //如果用户给与了这个权限 可以进行获取用户信息
+          wx.getUserInfo({
+            success: (response) => {
+              console.log(response.userInfo)
+              that.setData({
+                userInfo: response.userInfo
+              })
+              wx.login({
+                timeout: 50000,
+                success(loginRes){
+                  console.log(loginRes)
+                  /// 获取到code之后传给后台
+                  wx.request({
+                    url:  app.baseUrl + '/api/v2/wxlogin/',
+                    data:{
+                      'avatarUrl':response.userInfo.avatarUrl,
+                      'username': response.userInfo.nickName,
+                      'code': loginRes.code
+                    },
+                    method: "POST",
+                    header: {
+                      "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    success(userRes) {
+                      console.log(userRes);
+                    },fail(error){
+                      console.log(error);
+                    }
+                  })
+
+                },fail(error){
+
+                }
+              })
+            }
+          })
+        }else{
+          //如果用户没有给与这个权限则  发送询问权限的请求
+          wx.authorize({
+            scope:"scope.userInfo",//询问授权的属性
+            success:function(res){
+              console.log(res)
+            }
+          })
+        }
       }
     })
   },
